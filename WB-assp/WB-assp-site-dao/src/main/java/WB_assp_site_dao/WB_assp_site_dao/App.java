e478423bd045c8d92f54c91b3f32ca24d8e2c2cb
songhx <sleeponthestar@163.com>
2016-08-30 16:09:51 +0800

    【add】添加公用模块

---
+++ WB-assp/WB-assp-site-dao/src/main/java/WB_assp_site_dao/WB_assp_site_dao/App.java
@@ -0,0 +1,13 @@
+package WB_assp_site_dao.WB_assp_site_dao;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ WB-assp/WB-assp-tmpl-dao/src/main/java/WB_assp_tmpl_dao/WB_assp_tmpl_dao/App.java
@@ -0,0 +1,13 @@
+package WB_assp_tmpl_dao.WB_assp_tmpl_dao;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
